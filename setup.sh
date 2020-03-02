c7a46ad618b73778e2a0f2d0c7ad6cbb70138aba
Gabriel Meneses <gabriel.meneses@somoseducacao.com.br>
2020-03-02 16:20:20 -0300

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# Download Data
+(
+  mkdir data
+  cd data || exit
+  wget https://s3.amazonaws.com/data-sprints-eng-test/data-sample_data-nyctaxi-trips-2009-json_corrigido.json
+  wget https://s3.amazonaws.com/data-sprints-eng-test/data-sample_data-nyctaxi-trips-2010-json_corrigido.json
+  wget https://s3.amazonaws.com/data-sprints-eng-test/data-sample_data-nyctaxi-trips-2011-json_corrigido.json
+  wget https://s3.amazonaws.com/data-sprints-eng-test/data-sample_data-nyctaxi-trips-2012-json_corrigido.json
+  wget https://s3.amazonaws.com/data-sprints-eng-test/data-vendor_lookup-csv.csv
+  wget https://s3.amazonaws.com/data-sprints-eng-test/data-payment_lookup-csv.csv
+)
+
+
+# Create Infrastructure
+(
+  cd terraform || exit
+  terraform init
+  terraform refresh
+  terraform plan
+  terraform apply
+)
+
