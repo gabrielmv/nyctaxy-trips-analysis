89430eca0fad14389bca5f178f6f3828188aaad3
Gabriel Meneses <gabriel.meneses@somoseducacao.com.br>
2020-03-05 00:24:37 -0300

    update scripts

--- remote.sh
+++ remote.sh
@@ -1,7 +1,3 @@
-### Install Docker on remote machine
-export AWS_ACCESS_KEY_ID=${TF_VAR_aws_access_key_id}
-export AWS_SECRET_ACCESS_KEY=${TF_VAR_aws_secret_access_key}
-
 sudo apt upgrade -y
 sudo apt update -y
 sudo apt install python3-pip -y
@@ -9,10 +5,7 @@ sudo apt install python3 --upgrade -y
 
 pip3 install awscli --upgrade
 pip3 install boto3
-#aws configure
 
 git clone https://github.com/gabrielmv/nyctaxy-trips-analysis.git
 
 python3 nyctaxy-trips-analysis/streaming/kinesis_producer.py
-
-
